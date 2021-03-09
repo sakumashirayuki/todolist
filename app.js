15a37e7734bb2470f639bfa0fdb548558e903816
sakumashirayuki <2909549511@qq.com>
2021-03-09 21:40:18 +0800

    update

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const path = require("path");
+
 const date = require(path.join(__dirname, 'date.js'));
+const utils = require(path.join(__dirname, 'utils.js'));
 
 const app = express();
 
@@ -16,46 +18,72 @@ app.use(express.static("public"));
 // connect to mongoose
 mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connnection error:'));
-db.once('open',()=>{
-  const itemSchema = new mongoose.Schema({
-    name: String
-  });
-  const Item = mongoose.model("item", itemSchema);
-  // default items
-  const defaultItem1 = new Item({
-    name: 'Buy Food'
-  });
-  const defaultItem2 = new Item({
-    name: 'Cook Food'
-  });
-  const defaultItem3 = new Item({
-    name: 'Eat Food'
-  });
-  // Item.insertMany([defaultItem1, defaultItem2, defaultItem3]); // better to have a callback function
+const itemSchema = new mongoose.Schema({
+  name: String
 });
+const Item = mongoose.model("item", itemSchema);
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
 // const workItems = [];
+const defaultItem1 = new Item({
+  name: 'Buy Food'
+});
+const defaultItem2 = new Item({
+  name: 'Cook Food'
+});
+const defaultItem3 = new Item({
+  name: 'Eat Food'
+});
 
 app.get("/", function(req, res) {
+  const day = date.getDate();
+  const items = [];
+  Item.find({},(error,docs)=>{
+    if(error){
+      console.log(error);
+    }else{
+      if(docs.length==0){ // insert default list items
+        Item.insertMany([defaultItem1, defaultItem2, defaultItem3],(err)=>{
+          console.log(err);
+        });
+        res.redirect('/');
+      }else{
+        res.render("list", {listTitle: day, newListItems: docs});
+      }
+    }
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+  });
 });
 
+// post to add items
 app.post("/", function(req, res){
-
   const item = req.body.newItem;
   console.log(req.body.list);
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
   } else {
-    items.push(item);
+    const newItem = new Item({
+      name: item
+    });
+    newItem.save();
     res.redirect("/");
+  }
+});
+
+// post to delete items
+app.post("/delete", (req, res)=>{
+  if(!utils.isEmpty(req.body)){ // not click, not delete
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId,(err)=>{
+      if(err){
+        console.log(err);
+      }else{
+        console.log("successfully delete checked item!");
+      }
+    });
+    res.redirect('/');
+  }else{ // click, delete
+
   }
 });
 
---
+++ utils.js
@@ -0,0 +1,4 @@
+//jshint esversion:6
+exports.isEmpty = function(obj){
+    return Object.keys(obj).length === 0;
+};
